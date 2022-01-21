b27e1bcbc01d54a1e3ee3daaac933960457ea6e1
Anibal <apicazoq@minsait.com>
2022-01-21 13:34:43 +0100

    Initi II

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <ReaderSongs msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import ReaderSongs from './components/ReaderSongs.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ReaderSongs
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  padding: 2em;
+}
+</style>
---
+++ src/components/ReaderSongs.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="row">
+    <h1>Albums list</h1>
+    <div class="row">
+      <div class="col-md-3">
+        <div class="form-inline">
+        <input
+          class="form-control mr-sm-2"
+          type="search"
+          placeholder="Search"
+          aria-label="Search"
+          v-model="search"
+        />
+        <button
+          class="btn btn-primary"
+          @click="filterAlbum"
+        >
+          Search
+        </button>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-6" v-for="album in albums" :key="album.collectionId">
+      <div class="thumbnail">
+        <img :src="album.albumImage" alt="image" />
+        <div class="caption">
+          <h3>{{ album.collectionName }}</h3>
+          <p class="font-weight-bold">Price {{ album.price }}</p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+  
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "ReaderBases",
+  data() {
+    return {
+      albums: [],
+      search: "",
+    };
+  },
+  mounted() {
+    this.readSongs();
+  },
+  methods: {
+    async readSongs() {
+      const res = await axios.get(`http://localhost:3000/albums/`);
+      console.log("res", res);
+      this.albums = res.data;
+    },
+    async filterAlbum() {
+      const res = await axios.get(
+        `http://localhost:3000/albums/${this.search}`
+      );
+      console.log("res", res);
+      this.albums = res.data;
+    },
+  },
+};
+</script>
